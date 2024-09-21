62bd856d1036b1e5eb178d26765df23252732bb4
Trenton <130304830+Trentonom0r3@users.noreply.github.com>
2024-09-21 01:48:54 -0500

    fixed synchronization problem
    fixed synchronization problem

--- RifeTensorRT/include/interleave.h
+++
@@ -1,8 +0,0 @@
-// normalize.h
-#pragma once
-
-#include <cstdint>
-
-
-//void interleave_u_v(const uint8_t* u_buffer, const uint8_t* v_buffer, uint8_t* uv_buffer, int width, int height, cudaStream_t stream) {
-void interleave_u_v(const uint8_t* u_buffer, const uint8_t* v_buffer, uint8_t* uv_buffer, int width, int height);
--- RifeTensorRT/include/normalize.h
+++
@@ -1,6 +0,0 @@
-// normalize.h
-#pragma once
-
-#include <cstdint>
-
-void launch_normalize_kernel(const uint8_t* rgb, float* normalized, int total);
